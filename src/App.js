4a84b926036348ffb59eed9c6edfcaa081da325d
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-07-28 06:25:48 +0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const APP_ID = "e61a6aed";
-  const APP_KEY = "123c4b2f7a9e266be9f8b1cd1ce5e6d3";
+  const APP_ID = "e61a6aed"; //PLEASE USE YOUR OWN API DETAILS HERE
+  const APP_KEY = "123c4b2f7a9e266be9f8b1cd1ce5e6d3"; //AND HERE TOO..
+  const EXAMPLE_REQ = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
+  const [btnCounter, setbtnCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect run");
+  }, []);
   return (
     <div className="App">
       <h1>RECIPE APP</h1>
+      <form className="Search-Form">
+        <input className="Search-Bar" placeholder="Enter Dish"></input>
+        <button className="Search-Button" type="submit">
+          Search!
+        </button>
+      </form>
+      <h1 onClick={() => setbtnCounter(btnCounter + 1)}>{btnCounter}</h1>
     </div>
   );
 };
