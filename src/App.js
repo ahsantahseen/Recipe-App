eadd5b4a1c9f35cec21ae67d786056f4abdeeffc
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-07-28 06:42:29 +0500

    Designing the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Recipe from "./Recipe";
 
 const App = () => {
   const APP_ID = "e61a6aed"; //PLEASE USE YOUR OWN API DETAILS HERE
   const APP_KEY = "123c4b2f7a9e266be9f8b1cd1ce5e6d3"; //AND HERE TOO..
-  const EXAMPLE_REQ = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
   useEffect(() => {
     console.log("useEffect run");
+    getApiData();
   }, []);
+  const [Recipe, setRecipe] = useState([]);
+  const getApiData = async () => {
+    const Response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+    );
+    const Data = await Response.json();
+    setRecipe(Data.hits);
+  };
   return (
     <div className="App">
       <h1>RECIPE APP</h1>
