edc381475bcf827a939812ba6f14a6050f66e5c9
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-07-28 06:28:56 +0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ const App = () => {
   const APP_KEY = "123c4b2f7a9e266be9f8b1cd1ce5e6d3"; //AND HERE TOO..
   const EXAMPLE_REQ = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
 
-  const [btnCounter, setbtnCounter] = useState(0);
-
   useEffect(() => {
     console.log("useEffect run");
   }, []);
